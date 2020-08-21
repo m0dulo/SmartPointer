9dbc6a2ad599691565930ca28f18a12fb2cd17f8
m0dulo <cuckooegert@gmail.com>
2020-08-21 14:17:43 +0800

    add RefBase

---
+++ src/basic/refbase.h
@@ -0,0 +1,25 @@
+#ifndef REFBASE_H_
+#define REFBASE_H_
+
+class RefBase {
+private:
+    int mCount;
+public:
+    RefBase()
+        : mCount(0) {}
+
+    void incRefCount() {
+        mCount++;
+    }
+
+    int decRefCount() {
+        return --mCount;
+    }
+
+    int getRefCount() const {
+        return mCount;
+    }
+
+    virtual ~RefBase() {}
+};
+#endif // REFBASE_H_
